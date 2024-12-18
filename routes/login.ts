73c746532b3f945c44f01dbe6dffe45f812f8622
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2024-12-18 15:44:36 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- routes/login.ts
+++ routes/login.ts
@@ -33,13 +33,13 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    const email = req.body.email;
-    const pwd = req.body.password;
-    models.sequelize.findOne({ 
+    const email = req.body.email
+    const pwd = req.body.password
+    models.sequelize.findOne({
       where: {
-        email: email,
+        email,
         password: security.hash(pwd)
-      } 
+      }
     })
     // models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
       .then((authenticatedUser) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
